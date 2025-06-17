f8ba52e84a4fafcbe32687ab83dfbfd35a375b19
Ismael Pastor <ispaluan@gmail.com>
2025-06-17 16:32:48 +0200

    Coenzando React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
  //Aquí tu código
+ const [name, setName] = useState('Sofia');
+ const [newName, setNewName] = useState('');
+
+ return (
+     <div> 
+         <h2>Teacher Name: {name}</h2>
+         <ul>
+            <li onClick={() => setName('Data')} style={{cursor: 'pointer'}}>Data</li>
+            <li onClick={() => setName('Reyes')} style={{cursor: 'pointer'}}>Reyes</li>
+            <li onClick={() => setName('Yolanda')} style={{cursor: 'pointer'}}>Yolanda</li>
+         </ul>
+     </div>
+  )
 }
 
 export default App
+
